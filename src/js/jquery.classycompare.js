d874e1534b96489a6bc2349ce4749d6ed76dd8b4
cgat <chris.gat@gmail.com>
2015-04-25 16:41:27 -0700

    Add compare options: horizontal, vertical, and rectangle
    vertical and horizontal are as described. the Rectangle option is a combination of both vertical and horizontal, forming a rectangle that extends to the top left hand corner and cursor location.

--- src/js/jquery.classycompare.js
+++ src/js/jquery.classycompare.js
@@ -6,16 +6,22 @@
  * Licensed under the GPL Version 3 license.
  * Version 1.1.1
  *
+ * Additional functionality by M.E. Sanseverino
+ * Mar 11 2015
+ * added horizonal and rectangular wipes
  */
 (function ($) {
     $.fn.extend({
         ClassyCompare: function (b) {
             var c = {
                 defaultgap: "half",
-                leftgap: 10,
-                rightgap: 10,
-                caption: false,
-                reveal: .5
+                leftgap: 0,
+                rightgap: 0,
+                caption: true,
+                reveal: .5,
+                vertical: false,
+                horizontal: false,
+                rectangular: false
             };
             var b = $.extend(c, b);
             return this.each(function () {
@@ -32,22 +38,48 @@
                 $(this).append('<div class="uc-caption">' + $(this).children("img:eq(0)").attr("alt") + "</div>");
                 $(this).children(".uc-mask, .uc-bg").width(h);
                 $(this).children(".uc-mask, .uc-bg").height(i);
-                if(c.defaultgap=="half"){
-                    c.defaultgap = h/2;
+                if(c.horizontal){
+                    c.defaultgap = i/2;
+                    $(this).children(".uc-mask").animate({height: c.defaultgap}, "fast"); //changed from 1e3 to fast for the animation.
+                    $(this).children(".uc-mask").css("backgroundImage", "url(" + $(this).children("img:eq(0)").attr("src") + ")");
+					$(this).children(".uc-bg").css("backgroundImage", "url(" + $(this).children("img:eq(1)").attr("src") + ")");
+					    if (c.caption) {
+					       $(this).children(".uc-caption").show();
+					       }
                 }
                 else {
-                    c.defaultgap=h-c.defaultgap;
-                }
-                $(this).children(".uc-mask").animate({
-                    width: c.defaultgap
-                }, 1e3);
-                $(this).children(".uc-mask").css("backgroundImage", "url(" + $(this).children("img:eq(0)").attr("src") + ")");
-                $(this).children(".uc-bg").css("backgroundImage", "url(" + $(this).children("img:eq(1)").attr("src") + ")");
-                if (c.caption) {
-                    $(this).children(".uc-caption").show();
+                    c.defaultgap = h/2;
+                    $(this).children(".uc-mask").animate({width: c.defaultgap}, "fast"); //changed from 1e3 to fast for the animation.
+                    $(this).children(".uc-mask").css("backgroundImage", "url(" + $(this).children("img:eq(0)").attr("src") + ")");
+                    $(this).children(".uc-bg").css("backgroundImage", "url(" + $(this).children("img:eq(1)").attr("src") + ")");
+                        if (c.caption) {
+                       $(this).children(".uc-caption").show();
+				   }
                 }
             }).mousemove(function (c) {
                 var d = b;
+                if(d.horizontal)
+                {
+                	var pos_img = $(this).position()["top"];
+                	var new_height = c.pageY - pos_img;
+                	var img_height = $(this).height();
+                	var img_cap_one = $(this).children("img:eq(0)").attr("alt");
+               		var img_cap_two = $(this).children("img:eq(1)").attr("alt");
+                	if (new_height > d.leftgap && new_height < img_height - d.rightgap)
+                	{
+                    	$(this).children(".uc-mask").height(new_height)
+                	}
+                	if (new_height < img_height * d.reveal)
+                	{
+                    	$(this).children(".uc-caption").html(img_cap_two)
+                	}
+                	else
+                	{
+                    	$(this).children(".uc-caption").html(img_cap_one)
+                	}
+            	}
+                if (d.vertical)
+                {
                 var pos_img = $(this).position()["left"];
                 var new_width = c.pageX - pos_img;
                 var img_width = $(this).width();
@@ -62,6 +94,30 @@
                 else {
                     $(this).children(".uc-caption").html(img_cap_one)
                 }
+            	}
+                if (d.rectangular)
+                {
+                var pos_img = $(this).position()["left"];
+                var new_width = c.pageX - pos_img;  // setting the x value
+                //var new_width = c.pageX
+                var img_width = $(this).width();
+                var pos_img = $(this).position()["top"];
+                var new_height = c.pageY - pos_img;  // setting the y value
+                //var new_height = c.pageY;
+                var img_height = $(this).height();
+                var img_cap_one = $(this).children("img:eq(0)").attr("alt");
+                var img_cap_two = $(this).children("img:eq(1)").attr("alt");
+                if (new_width > d.leftgap && new_width < img_width - d.rightgap) {
+                    $(this).children(".uc-mask").width(new_width)
+                    $(this).children(".uc-mask").height(new_height)
+                }
+                if (new_width < img_width * d.reveal) {
+                    $(this).children(".uc-caption").html(img_cap_two)
+                }
+                else {
+                    $(this).children(".uc-caption").html(img_cap_one)
+                }
+            	}
             })
         }
     })
